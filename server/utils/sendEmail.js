8b405666ff3f21b96368e0b2377f81d0ef6bf51b
D3V <xdave053@gmail.com>
2026-09-05 15:09:19 +0530

    Solving email service problem

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,9 +1,10 @@
 const nodemailer = require("nodemailer");
-
+const port = Number(process.env.EMAIL_PORT) || 587;
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT) || 587,
+    port,
+    secure: port === 587,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
